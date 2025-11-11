0cb41da9fda122125c38d49bb5d12c3b23c490ce
Gerrit Roska <gerritroska@gmail.com>
2025-11-11 16:07:42 -0500

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Gerrit Roska on 11/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("i dont like")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Gerrit Roska on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
